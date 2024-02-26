35a3e127fd0daa4be6019b50d5189a96cc5da339
Douwe Pausma <douwepausma@gmail.com>
2024-02-26 14:42:06 +0100

    Create Typography component

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode } from "react";
+// import './Button.scss';
+
+// /**
+//  * @typedef Props
+//  * @prop {string} className Pass any custom classes
+//  * @prop {string} color Color of the button
+//  * @prop {string} variant Variant can be full | outline | icon
+//  * @prop {string} size Size can be normal | small
+//  * @prop {React.ReactNode} children
+//  * @prop {React.ReactNode} as Take on a different element than button
+//  * @prop {object} attributes Pass any other non-specified properties
+//  */
+
+// /**
+//  * A button looking element acting as button by default
+//  * @param {Props} props 
+//  * @returns 
+//  */
+
+export interface Typography {
+    children: ReactNode
+    className: string,
+    as: any
+    attributes: object,
+    size: string,
+    noMaxWidth: boolean
+}
+
+const Typography = ({
+  children,
+  className = '',
+  as = 'div',
+  attributes,
+  size = 'base',
+  noMaxWidth = true,
+}: Typography) => {
+  const TypographyElement:Typography["as"] = as;
+  const sizeClases = {
+    'sm': 'prose-sm',
+    'base': 'prose-base',
+    'lg': 'prose-base lg:prose-lg',
+    'xl': 'prose-base lg:prose-xl',
+  }
+
+  return (
+    <TypographyElement
+      className={`prose ${sizeClases[size]} ${noMaxWidth ? 'max-w-none' : ''} ${className}`}
+      {...attributes}
+    >
+      {children}
+    </TypographyElement>
+  )
+};
+
+export default Typography;
