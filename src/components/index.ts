4194098dff148309d25d94a5e2967dc7cc3a9bef
Douwe Pausma <douwepausma@gmail.com>
2024-03-04 15:14:48 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,10 @@
 export { default as Badge } from "./Badge";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as FieldWrapper } from "./FieldWrapper";
 export { default as Heading } from "./Heading";
+export { default as CardImage } from "./CardImage";
 export { default as Kicker } from "./Kicker";
+export { default as SelectField } from "./SelectField";
 export { default as Typography } from "./Typography";
 export { default as Logo } from "./Logo";
