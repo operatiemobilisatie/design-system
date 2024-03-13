4bc81fd71012f7eb58ca8bc71a4abbf1e3089cad
Douwe Pausma <douwepausma@gmail.com>
2024-03-13 14:23:05 +0100

    Add Formik compatibility

--- src/components/SelectField/SelectField.tsx
+++ src/components/SelectField/SelectField.tsx
@@ -1,22 +1,52 @@
-import React, { ReactNode } from "react";
+import React, { useId, ReactNode } from "react";
 import Select from 'react-select';
 import './SelectField.scss'
 
 export interface SelectField {
   className: String,
-  children: ReactNode
-  attributes: Object,
+  children: ReactNode,
+  attributes: object,
   size: 'sm' | 'md' | 'lg'
 }
+
+const getValue = ({options, field}) => {
+  if(options && field.value) {
+    return options.filter(option => field.value.indexOf(option.value) >= 0);
+  } else {
+    return [];
+  }
+}
+
+const onChange = (props, values) => {
+  if(props.form && props.field) {
+    props.form.setFieldValue(props.field.name, (
+      // If isMulti -> map values
+      props.isMulti ? (
+        values.map((option) => option.value)
+      ) : (
+        [values.value]
+      )
+    ))
+  }
+}
+
 
 const SelectField = (props: SelectField) => {
   const {size = 'md'} = props;
   
   return (
     <Select
+      // When using Formik -> get initial values
+      value={getValue(props)}
+      
+      // When using Formik -> update form
+      onChange={(values) => onChange(props, values)}
+      
       {...props}
+    
       classNamePrefix="om-select"
       unstyled
+      instanceId={useId()}
       className={`om-select-${size}`}
     />
   )
