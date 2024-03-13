86bd2e94bc525374a98d390fb6ffa4a64e6b1092
Douwe Pausma <douwepausma@gmail.com>
2024-03-13 14:24:35 +0100

    Improve field wrapper

--- src/components/FieldWrapper/FieldWrapper.tsx
+++ src/components/FieldWrapper/FieldWrapper.tsx
@@ -3,21 +3,27 @@ import './FieldWrapper.scss';
 
 export interface FieldWrapper {
   className: String,
-  children: ReactNode
+  children: ReactNode,
+  style: object,
   attributes: Object,
   as: any,
   label: string,
   fieldId: string,
+  required: boolean,
+  errorMessage: string,
   size: 'sm' | 'md' | 'lg'
 }
 
 const FieldWrapper = ({
   children,
   className = '',
+  style,
   attributes,
   as = 'div',
   label,
   fieldId,
+  required,
+  errorMessage,
   size
 }: FieldWrapper) => {
   const FieldWrapperElement:FieldWrapper["as"] = as;
@@ -26,9 +32,20 @@ const FieldWrapper = ({
     <FieldWrapperElement
       className={`field-wrapper ${size ? `field-wrapper-${size}` : ''} ${className}`}
       {...attributes}
+      style={style}
     >
-      {label && <label htmlFor={fieldId}>{label}</label>}
+      {label && (
+        <label htmlFor={fieldId}>
+          {label}
+          {required && <sup>*</sup>}
+        </label>
+      )}
       {children}
+      <small className="text-primary">
+        <i>
+          {errorMessage}
+        </i>
+      </small>
     </FieldWrapperElement>
   )
 };
