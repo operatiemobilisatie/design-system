793f9f8e8c16989d3a340246614a098f787379e0
Douwe Pausma <douwepausma@gmail.com>
2024-03-13 14:23:29 +0100

    Remove fragment tag

--- src/components/CardImage/CardImage.tsx
+++ src/components/CardImage/CardImage.tsx
@@ -17,15 +17,13 @@ const CardImage = ({
   image
 }: CardImage) => {
   return (
-    <>
-      <div className={`card-image ${!fill ? 'overflow-clip relative aspect-video -mx-8 -mt-8 mb-8' : ''}`}>
-        <Image src={image} alt={alt} fill={true} />
-        {children && <div className={`absolute inset-0 ${!fill ? 'bg-gradient-to-t from-black/20 to-transparent' : 'bg-black/40'} ${className}`}></div>}
-        <div className={`relative drop-shadow-sm text-white ${!fill ? 'h-full flex items-end p-8' : ''}`}>
-          {children}
-        </div>
+    <div className={`card-image ${!fill ? 'overflow-clip relative aspect-video -mx-8 -mt-8 mb-8' : ''} ${className}`}>
+      <Image src={image} alt={alt} fill={true} />
+      {children && <div className={`absolute inset-0 ${!fill ? 'bg-gradient-to-t from-black/20 to-transparent' : 'bg-black/40'} ${className}`}></div>}
+      <div className={`relative drop-shadow-sm text-white ${!fill ? 'h-full flex items-end px-8 py-6' : ''}`}>
+        {children}
       </div>
-    </>
+    </div>
   )
 };
 
