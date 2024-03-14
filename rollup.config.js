6510af343479eb014894cc74f5e41f65fff8e8b8
Douwe Pausma <douwepausma@gmail.com>
2024-03-14 15:06:27 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,33 +5,82 @@ import dts from "rollup-plugin-dts";
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-import packageJson from "./package.json" assert { type: "json" };
+// import packageJson from "./package.json" assert { type: "json" };
 
 export default [
+
+  // Server Components
   {
-    input: ["src/index.ts"],
+    input: ["src/components/server/index.ts"],
     output: [
       {
-        file: packageJson.main,
+        file: './dist/server/index.js',
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: './dist/server/index.js',
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
-      postcss({
-        extract: 'css/style.css'
-      }),
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "tsconfig.json" })
+      typescript({ tsconfig: "tsconfig.json" }),
+      postcss({
+        extract: false,
+        inject: false
+      })
     ],
   },
+
+  // Client Components
+  {
+    input: ["src/components/client/index.ts"],
+    output: [
+      {
+        file: './dist/client/index.js',
+        format: "cjs",
+        sourcemap: true,
+        banner: "'use client';"
+      },
+      {
+        file: './dist/client/index.js',
+        format: "esm",
+        sourcemap: true,
+        banner: "'use client';"
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "tsconfig.json" }),
+      postcss({
+        extract: false,
+        inject: false
+      })
+    ],
+  },
+
+  // Styling
+  {
+    input: "src/index.ts",
+    output: [{file: "dist/index.js", format: "esm"}],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "tsconfig.json" }),
+      postcss({
+        extract: "css/style.css"
+      })
+    ]
+  },
+
+  // Tailwind
   {
     input: "src/tailwindcss/index.ts",
     output: [{file: "dist/tailwindcss/index.js", format: "esm"}],
@@ -41,10 +90,12 @@ export default [
       typescript(),
     ]
   },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.scss$/]
-  },
+
+  // Types
+  // {
+  //   input: "dist/types/index.d.ts",
+  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
+  //   plugins: [dts()],
+  //   external: [/\.scss$/]
+  // },
 ];
