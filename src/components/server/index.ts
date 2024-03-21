fe611b11f57be0201163be8ee53254a1055708fb
Douwe Pausma <douwepausma@gmail.com>
2024-03-21 13:09:46 +0100

    Update index.ts

--- src/components/server/index.ts
+++ src/components/server/index.ts
@@ -1,10 +1,14 @@
 export { default as Badge } from "./Badge";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as CardImage } from "./CardImage";
+export { default as Checkbox } from "./Checkbox";
+export { default as FieldGroup } from "./FieldGroup";
 export { default as FieldWrapper } from "./FieldWrapper";
 export { default as Heading } from "./Heading";
-export { default as CardImage } from "./CardImage";
 export { default as Kicker } from "./Kicker";
-export { default as Typography } from "./Typography";
-export { default as Logo } from "./Logo";
 export { default as Loading } from "./Loading";
+export { default as Logo } from "./Logo";
+export { default as Radio } from "./Radio";
+export { default as Typography } from "./Typography";
+
